4c3aba7e8dd543eed46a771518af121143e7ccac
JeycelPerdomo <jeycelperdomo@gmail.com>
2021-12-21 18:10:58 -0300

    Subo el mercado liebre a git hub

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const { dirname } = require('path');
+
+const path = require ('path')
+const app = express();
+
+app.listen(3000, () => console.log ('servidor corriendo en el puerto 3000'))
+
+app.use (express.static(path.resolve(__dirname, 'public')))
+
+app.get ('/', (req,res) => {
+    let htmlPath= path.resolve (__dirname, 'views/home.html')
+    res.sendFile(htmlPath);
+})
+
+app.get ('/register', (req,res) => {
+    let htmlPath2 = path.resolve (__dirname, 'views/register.html');
+    res.sendFile (htmlPath2)
+})
+
+app.get ('/login', (req,res) => {
+    let htmlPath3 = path.resolve (__dirname, 'views/login.html');
+    res.sendFile (htmlPath3)
+})
